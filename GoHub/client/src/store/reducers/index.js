d3fbb8ec78fc9a37433e61ecc35caf1a76f21a9b
karandevtyagi <vatstyagi999@gmail.com>
2019-10-07 23:44:01 +0530

    30per

--- GoHub/client/src/store/reducers/index.js
+++ GoHub/client/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import isLogged from './isLogged';
 import alert from './alert';
+import auth from './auth';
 
 const allReducers = combineReducers({
   isLogged,
   alert,
+  auth,
 });
 export default allReducers;
